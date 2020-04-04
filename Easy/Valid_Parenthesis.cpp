b20311ef75b1e1cfa802c826ed14005ecf22c999
JeremyTaraba <jeremytaraba@gmail.com>
2020-04-03 21:46:16 -0700

    Added Valid Parenthesis

---
+++ Easy/Valid_Parenthesis.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> leftP;
+        for(int i = 0; i < s.length(); i++){
+            
+            if(s.at(i) == '(' || s.at(i) == '{' || s.at(i) == '['){
+                leftP.push(s.at(i));
+            }
+            
+            else if (s.at(i) == ')' || s.at(i) == '}' || s.at(i) == ']'){
+                if(leftP.empty()){
+                    return false;
+                }
+                
+                if(leftP.top() == '(' && s.at(i) == ')' ){  
+                    leftP.pop();
+                }
+                
+                else if(leftP.top() == '[' && s.at(i) == ']' ){  
+                    leftP.pop();
+                }
+                
+                else if(leftP.top() == '{' && s.at(i) == '}' ){  
+                    leftP.pop();
+                }
+                
+                else{
+                    return false;
+                }
+            }
+            
+        }
+        
+        if(leftP.empty()){
+            return true;
+        }
+        return false;
+        
+    }
+};
