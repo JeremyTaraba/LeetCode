f82788a399b562f4b45bad0b0c5169900549e7af
JeremyTaraba <jeremytaraba@gmail.com>
2020-04-23 22:48:11 -0700

    Create Split_A_String_In_Balanced_Strings.cpp

---
+++ Easy/Split_A_String_In_Balanced_Strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int stringCount = 0;
+        int RLCount = 0;
+        for(int i = 0; i < s.size(); i++){
+            for(int j = i; j < s.size(); j++){
+                if(s.at(j) == 'L'){
+                    RLCount--;
+                }
+                else if(s.at(j) == 'R'){
+                    RLCount++;
+                }
+                if(RLCount == 0){
+                    i = j;
+                    stringCount++;
+                }
+            }
+            
+        }
+        return stringCount;
+    }
+};
