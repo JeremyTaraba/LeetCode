638840a78536867770f615c6f4e9780d182bb724
JeremyTaraba <jeremytaraba@gmail.com>
2020-05-01 23:13:19 -0700

    Create unique_number_of_occurrences.cpp

---
+++ Easy/unique_number_of_occurrences.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        vector<int> occ;
+        //first sort the array then count number of occurences
+        //then sort occurences and check if any values next to each other
+        // are the same
+        for(int i = 0; i < arr.size(); i++){
+            
+        }
+    }
+};
