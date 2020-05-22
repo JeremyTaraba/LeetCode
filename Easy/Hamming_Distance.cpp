461861c33ffb2ebd5799c3c9a33c3569763f5db3
JeremyTaraba <jeremytaraba@gmail.com>
2020-05-21 22:57:57 -0700

    Create Hamming_Distance.cpp

---
+++ Easy/Hamming_Distance.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int binaryArrayX[32] = {0};
+        int binaryArrayY[32] = {0};
+        int i = 0;
+        int hammingDistance = 0;
+        
+        while(x > 0){
+            binaryArrayX[i] = x % 2; 
+            x = x / 2; 
+            i++; 
+        }
+        i = 0;
+        while(y > 0){
+            binaryArrayY[i] = y % 2; 
+            y = y / 2; 
+            i++; 
+        }
+        
+   
+        for (int j = 31; j >= 0; j--) {  //reverse this
+            if(binaryArrayX[j] != binaryArrayY[j] ){
+                hammingDistance++;
+            }
+          
+        }
+        
+        return hammingDistance;
+    }
+    
+};
