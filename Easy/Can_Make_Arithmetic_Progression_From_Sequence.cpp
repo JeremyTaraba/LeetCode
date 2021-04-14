2e42bce37c80233765b34c8481c66b0a4e67cf0a
JeremyTaraba <jeremytaraba@gmail.com>
2021-04-13 18:29:14 -0700

    Update Can_Make_Arithmetic_Progression_From_Sequence.cpp

--- Easy/Can_Make_Arithmetic_Progression_From_Sequence.cpp
+++ Easy/Can_Make_Arithmetic_Progression_From_Sequence.cpp
@@ -1,5 +1,12 @@
+/*
+Prompt:
+Given an array of numbers arr. A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
+
+Return true if the array can be rearranged to form an arithmetic progression, otherwise, return false.
 
 
+ 
+*/
 
 
 
