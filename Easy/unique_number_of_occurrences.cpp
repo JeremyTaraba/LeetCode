7ed3ce09b62271e3b25f05a59ee280e5b97f9324
JeremyTaraba <jeremytaraba@gmail.com>
2021-04-15 20:44:01 -0700

    Update unique_number_of_occurrences.cpp

--- Easy/unique_number_of_occurrences.cpp
+++ Easy/unique_number_of_occurrences.cpp
@@ -1,15 +1,34 @@
-#include <vector>
-
-
 class Solution {
 public:
+    bool checkOccurrences(vector<int>& occur, int numOccur){
+        for(int k = 0; k < occur.size(); k++){
+                if(numOccur == occur.at(k)){
+                    return false;   //occurs the same amount of time as something else
+                }
+            }
+        return true;
+    }
+        
     bool uniqueOccurrences(vector<int>& arr) {
-        vector<int> occ;
-        //first sort the array then count number of occurences
-        //then sort occurences and check if any values next to each other
-        // are the same
-        for(int i = 0; i < arr.size(); i++){
-            
+        sort(arr.begin(), arr.end());
+        arr.push_back(arr.at(arr.size()-1) + 1); //add a different value then the ending value
+        vector<int> occurrences;
+        int numOccur = 0;
+        
+        for(int i = 0; i < arr.size()-1; i++){
+            if(arr.at(i) != arr.at(i+1)){
+                numOccur++;
+                if(!checkOccurrences(occurrences, numOccur)){
+                    return false;
+                }
+                occurrences.push_back(numOccur);
+                numOccur = 0;
+            }
+            else{
+                numOccur++;
+            }            
         }
+        
+        return true;
     }
 };
