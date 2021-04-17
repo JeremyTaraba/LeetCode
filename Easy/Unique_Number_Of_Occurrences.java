502a377c46ef2ab3db73294a65bc36b437c7d1c9
JeremyTaraba <jeremytaraba@gmail.com>
2021-04-17 16:50:38 -0700

    Update Unique_Number_Of_Occurrences.java

--- Easy/Unique_Number_Of_Occurrences.java
+++ Easy/Unique_Number_Of_Occurrences.java
@@ -1,3 +1,17 @@
+/*
+Prompt:
+Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
+
+Example:
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+
+*/
+
+
+
 class Solution {
     public boolean checkOccurrences(List<Integer> occur, int numOccur){
         for(int i = 0; i < occur.size(); i++){
