f384dad27565bca8832799520272609cb4a4524a
JeremyTaraba <jeremytaraba@gmail.com>
2021-04-16 17:16:13 -0700

    Update Unique_Number_Of_Occurrences.cs

--- Easy/Unique_Number_Of_Occurrences.cs
+++ Easy/Unique_Number_Of_Occurrences.cs
@@ -1,3 +1,16 @@
+/*
+Prompt:
+Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
+
+Example:
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+
+*/
+
+
 public class Solution {
     public bool NumOccurrences(List<int> occur, int numOccur){
         foreach(int i in occur){
