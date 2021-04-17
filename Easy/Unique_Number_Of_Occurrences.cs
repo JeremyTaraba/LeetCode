b397125b54ecc9ee6bf29dcc2ba4f23a23f2e268
JeremyTaraba <jeremytaraba@gmail.com>
2021-04-16 17:14:31 -0700

    Create Unique_Number_Of_Occurrences.cs

---
+++ Easy/Unique_Number_Of_Occurrences.cs
@@ -0,0 +1,51 @@
+public class Solution {
+    public bool NumOccurrences(List<int> occur, int numOccur){
+        foreach(int i in occur){
+            if(i == numOccur){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    
+    public bool UniqueOccurrences(int[] arr) {
+        List<int> occur = new List<int>();
+        int numOccur = 0;
+        Array.Sort(arr);
+        
+        int[] tempArr = new int[arr.Length + 1];
+        arr.CopyTo(tempArr, 0);
+        tempArr[arr.Length] = arr[arr.Length - 1] + 1;
+        
+        for(int i = 0; i < tempArr.Length-1; i++) {
+            if(tempArr[i] != tempArr[i+1]){
+                if(!NumOccurrences(occur, numOccur)){
+                    return false;
+                }
+                else{
+                    occur.Add(numOccur);
+                    numOccur = 0;
+                }
+            }
+            else{
+                numOccur++;
+            }
+        }
+        
+        return true;
+    }
+}
+
+/*
+Note:
+Can't use foreach as the main loop since we need the current and next array element.
+As a side note, running this in cpp gives a runtime of 4ms and 7.8mb of memory while running in c# gives 92ms and 24.7mb of memory.
+Can defininitely optimize the c# code to be faster but not as fast as the cpp code.
+
+Constraints:
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000
+
+*/
