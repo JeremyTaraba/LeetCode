9345da43fb748ac8f481aa3705d129cbc5cf4753
JeremyTaraba <jeremytaraba@gmail.com>
2021-04-22 18:51:41 -0700

    Update Maximum_Number_Of_Balls_In_A_Box.cs

--- Easy/Maximum_Number_Of_Balls_In_A_Box.cs
+++ Easy/Maximum_Number_Of_Balls_In_A_Box.cs
@@ -1,3 +1,21 @@
+/*
+Prompt:
+You are working in a ball factory where you have n balls numbered from lowLimit up to highLimit inclusive (i.e., n == highLimit - lowLimit + 1), and an infinite number of boxes numbered from 1 to infinity.
+
+Your job at this factory is to put each ball in the box with a number equal to the sum of digits of the ball's number. For example, the ball number 321 will be put in the box number 3 + 2 + 1 = 6 and the ball number 10 will be put in the box number 1 + 0 = 1.
+
+Given two integers lowLimit and highLimit, return the number of balls in the box with the most balls.
+
+ 
+Example:
+
+
+
+
+*/
+
+
+
 public class Solution {
     public int CountBalls(int lowLimit, int highLimit) {
         int[] boxes = new int[46];    //arrays are initialized as 0's in c#, Max number of boxes possible is 46
