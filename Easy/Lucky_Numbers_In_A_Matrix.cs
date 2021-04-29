0610b0da70832c8bd052bee678b56be4e1c32cc2
Jeremy Taraba <jeremytaraba@gmail.com>
2021-04-28 22:20:44 -0700

    Create Lucky_Numbers_In_A_Matrix.cs

---
+++ Easy/Lucky_Numbers_In_A_Matrix.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public IList<int> LuckyNumbers (int[][] matrix) {
+        int rowLength = matrix.Length;
+        int columnLength = matrix[0].Length;
+        List<int> numbers = new List<int>();
+        
+        int min;
+        int minIndex;
+        int max;
+        int maxIndex;
+        for(int i = 0; i < rowLength; i++){
+            minIndex = 0;
+            maxIndex = 0;
+            min = matrix[i][0];
+            for(int j = 0; j < columnLength; j++){
+                if(min > matrix[i][j]){
+                    min = matrix[i][j];
+                    minIndex = j;
+                }
+            }
+            max = matrix[0][minIndex];
+            for(int k = 0; k < rowLength; k++){
+                if(max < matrix[k][minIndex]){
+                    max = matrix[k][minIndex];
+                    maxIndex = k;
+                }
+            }
+            
+            if(matrix[i][minIndex] == matrix[maxIndex][minIndex]){
+                numbers.Add(min);
+            }
+        }
+        
+        return numbers;
+    }
+}
