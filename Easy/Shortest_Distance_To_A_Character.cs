0a949616754add8ec43fe3dfcedfd3114ef4f619
Jeremy Taraba <jeremytaraba@gmail.com>
2021-04-29 23:49:20 -0700

    Update Shortest_Distance_To_A_Character.cs

--- Easy/Shortest_Distance_To_A_Character.cs
+++ Easy/Shortest_Distance_To_A_Character.cs
@@ -1,3 +1,16 @@
+/*
+Prompt:
+Given a string s and a character c that occurs in s, return an array of integers answer where answer.length == s.length and answer[i] is the distance from index i to the closest occurrence of character c in s.
+
+The distance between two indices i and j is abs(i - j), where abs is the absolute value function.
+
+Example:
+Input: s = "loveleetcode", c = "e"
+Output: [3,2,1,0,1,0,0,1,2,2,1,0]
+
+*/
+
+
 public class Solution {
     public int[] ShortestToChar(string s, char c) {
         int[] answer = new int[s.Length];
