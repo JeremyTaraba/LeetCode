5926eb717669918ee2ab525009fab255d59650ca
Jeremy Taraba <jeremytaraba@gmail.com>
2021-05-07 12:19:15 -0700

    Update Remove_All_Adjacent_Duplicates_In_String.cs

--- Easy/Remove_All_Adjacent_Duplicates_In_String.cs
+++ Easy/Remove_All_Adjacent_Duplicates_In_String.cs
@@ -1,19 +1,24 @@
 public class Solution {
     public string RemoveDuplicates(string S) {
-        bool removeDup = true;
+        Stack<char> stack = new Stack<char>();
         
-        while(removeDup){
-            removeDup = false;
-            for(int i = 0; i < S.Length-1; i++){
-                if(S[i] == S[i+1]){
-                    removeDup = true;
-                    S = S.Remove(i, 2);
-                    //Console.WriteLine(S);
-                    break;
-                }
-            } 
-        } 
         
-        return S;
+        foreach(char c in S){
+            if(stack.Count > 0 && stack.Peek() == c){
+                stack.Pop();
+            }
+            else{
+                stack.Push(c);
+            }
+                
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(stack.Count > 0){
+            sb.Append(stack.Pop());
+        }
+        
+        return new string(sb.ToString().Reverse().ToArray());
     }
 }
+
