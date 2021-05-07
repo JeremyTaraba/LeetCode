4d9e9ae789a2254976ff4dd5b313e27f42a4fc21
Jeremy Taraba <jeremytaraba@gmail.com>
2021-05-07 12:20:52 -0700

    Update Remove_All_Adjacent_Duplicates_In_String.cs

--- Easy/Remove_All_Adjacent_Duplicates_In_String.cs
+++ Easy/Remove_All_Adjacent_Duplicates_In_String.cs
@@ -1,3 +1,14 @@
+/*
+Prompt:
+Given a string S of lowercase letters, a duplicate removal consists of choosing two adjacent and equal letters, and removing them.
+
+We repeatedly make duplicate removals on S until we no longer can.
+
+Return the final string after all such duplicate removals have been made.  It is guaranteed the answer is unique.
+
+
+*/
+
 public class Solution {
     public string RemoveDuplicates(string S) {
         Stack<char> stack = new Stack<char>();
