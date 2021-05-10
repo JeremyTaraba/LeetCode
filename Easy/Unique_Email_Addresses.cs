83ec3e48525c66ada43d03fec39fcf02d418715e
Jeremy Taraba <jeremytaraba@gmail.com>
2021-05-10 16:28:53 -0700

    Update Unique_Email_Addresses.cs

--- Easy/Unique_Email_Addresses.cs
+++ Easy/Unique_Email_Addresses.cs
@@ -18,3 +18,19 @@ public class Solution {
     
     }
 }
+
+/*
+Notes:
+At first I was going to parse the emails into a sorted list and just check how many unique emails were in that list
+But this is the perfect case to use a hashset. A hashset will keep each entry unique because it hashes the 
+data before it is entered into the list. Those with the same name will be hashed the same way and put into the same bucket
+
+Constraints:
+1 <= emails.length <= 100
+1 <= emails[i].length <= 100
+email[i] consist of lowercase English letters, '+', '.' and '@'.
+Each emails[i] contains exactly one '@' character.
+All local and domain names are non-empty.
+Local names do not start with a '+' character.
+
+*/
