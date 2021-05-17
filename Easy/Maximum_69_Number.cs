98eb789901a076f196376a625125da4abaedc090
Jeremy Taraba <jeremytaraba@gmail.com>
2021-05-16 22:50:19 -0700

    Update Maximum_69_Number.cs

--- Easy/Maximum_69_Number.cs
+++ Easy/Maximum_69_Number.cs
@@ -10,7 +10,7 @@ public class Solution {
     public int Maximum69Number (int num) {
         String numberString = num.ToString();
         StringBuilder sb = new StringBuilder(numberString);
-
+ 
         for (int i = 0; i < numberString.Length; i++){       
             if(sb[i] == '6'){
                 sb[i] = '9';
