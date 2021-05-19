cd8c63dc44dad058091c6e79f71d4cbf2ee9ef2b
Jeremy Taraba <jeremytaraba@gmail.com>
2021-05-19 15:06:33 -0700

    Update Find_N_Unique_Integers_Sum_up_to_Zero.py

--- Easy/Find_N_Unique_Integers_Sum_up_to_Zero.py
+++ Easy/Find_N_Unique_Integers_Sum_up_to_Zero.py
@@ -1,3 +1,11 @@
+"""
+Prompt:
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+
+"""
+
+
 class Solution(object):
     def sumZero(self, n):
         """
@@ -18,3 +26,12 @@ class Solution(object):
             result.append(0)
             return result
         
+"""
+Notes:
+looping for n/2 since we append twice for each iteration. One append for the positive integer and one for the negative interger
+so the sum equals zero after every append. If n is odd then we append a zero at the end to keep the sum zero but have a large enough array
+
+Constraints:
+1 <= n <= 1000
+
+"""
