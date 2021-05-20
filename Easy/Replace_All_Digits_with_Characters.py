4ef678256137b550c0a9367325bb69979bb635e5
Jeremy Taraba <jeremytaraba@gmail.com>
2021-05-20 14:44:02 -0700

    Update Replace_All_Digits_with_Characters.py

--- Easy/Replace_All_Digits_with_Characters.py
+++ Easy/Replace_All_Digits_with_Characters.py
@@ -13,3 +13,15 @@ class Solution(object):
                 str1+=chr(ord(x) + int(char))
             
         return str1
+
+"""
+Notes:
+runtime O(n) 
+ord returns the integer of a unicode character then we add that to the shift. Then chr returns a character from an integer
+
+Constraints:
+1 <= s.length <= 100
+s consists only of lowercase English letters and digits.
+shift(s[i-1], s[i]) <= 'z' for all odd indices i
+
+"""
