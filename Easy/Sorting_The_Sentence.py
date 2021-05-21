0a62d3167e341332783d8b9df724322b6ea585f5
Jeremy Taraba <jeremytaraba@gmail.com>
2021-05-21 15:40:25 -0700

    Update Sorting_The_Sentence.py

--- Easy/Sorting_The_Sentence.py
+++ Easy/Sorting_The_Sentence.py
@@ -1,3 +1,14 @@
+"""
+Prompt:
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
+
+A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+
+For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
+Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
+
+"""
+
 class Solution(object):
     def sortSentence(self, s):
         """
