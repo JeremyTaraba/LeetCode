83a9b4c7e0cc86ad7da45786f647298d068f4da3
Jeremy Taraba <jeremytaraba@gmail.com>
2021-05-21 15:40:04 -0700

    Update Sorting_The_Sentence.py

--- Easy/Sorting_The_Sentence.py
+++ Easy/Sorting_The_Sentence.py
@@ -7,8 +7,26 @@ class Solution(object):
         str1=s.split()
         orderList=[""]*len(str1)
         for word in str1:
-            justString=word[:-1] #remove last character which is number
+            justString=word[:-1]  #remove the last character which is the number
             orderList[int(word[-1])-1]=justString   #str[-1] goes to last character in string
             ans=" ".join(orderList)
 
         return ans
+
+
+"""
+Notes:
+We create an empty list[] that has the size of how many words are in our sentence 
+Then we place the words into the list based on their index.
+O(n) time. 
+
+
+Constraints:
+2 <= s.length <= 200
+s consists of lowercase and uppercase English letters, spaces, and digits from 1 to 9.
+The number of words in s is between 1 and 9.
+The words in s are separated by a single space.
+s contains no leading or trailing spaces.
+
+
+"""
