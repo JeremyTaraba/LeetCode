d77099e24334e0c0070eca562b23a4508265986b
Jeremy Taraba <jeremytaraba@gmail.com>
2021-05-24 16:31:12 -0700

    Update Determine_Color_of_a_Chessboard_Square.py

--- Easy/Determine_Color_of_a_Chessboard_Square.py
+++ Easy/Determine_Color_of_a_Chessboard_Square.py
@@ -13,3 +13,19 @@ class Solution(object):
         second = int(coordinates[1]) % 2 == 0
             
         return first != second  # logical xor gate
+
+
+"""
+Notes:
+We have a string "a1", think of this in terms of numbers we have odd numbers being false and even
+numbers being true. First I convert the first character in the string 'a' to a number. where a = 1, b = 2
+and so on. Our new string is "11" check each number to see if it is even or odd. Two odd numbers is a black square
+two even numbers is a black square and an odd and even number will always be a white square. This is a Xor operation.
+
+
+Constraints:
+coordinates.length == 2
+'a' <= coordinates[0] <= 'h'
+'1' <= coordinates[1] <= '8'
+
+"""
