f805af98ec65fabf3ed21fb3a0dc699493ff9a92
Jeremy Taraba <jeremytaraba@gmail.com>
2021-05-24 16:31:44 -0700

    Update Determine_Color_of_a_Chessboard_Square.py

--- Easy/Determine_Color_of_a_Chessboard_Square.py
+++ Easy/Determine_Color_of_a_Chessboard_Square.py
@@ -1,3 +1,15 @@
+"""
+Prompt:
+You are given coordinates, a string that represents the coordinates of a square of the chessboard.
+
+Return true if the square is white, and false if the square is black.
+
+The coordinate will always represent a valid chessboard square. The coordinate will always have the letter first, and the number second.
+
+"""
+
+
+
 class Solution(object):
     def squareIsWhite(self, coordinates):
         """
