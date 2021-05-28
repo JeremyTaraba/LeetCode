1c9c771565192abb5da13b9c98df51d5eda0a42d
Jeremy Taraba <jeremytaraba@gmail.com>
2021-05-28 15:54:45 -0700

    Update Minimum_Operations_to_Make_the_Array_Increasing.py

--- Easy/Minimum_Operations_to_Make_the_Array_Increasing.py
+++ Easy/Minimum_Operations_to_Make_the_Array_Increasing.py
@@ -12,3 +12,19 @@ class Solution(object):
                 nums[i+1] = nums[i] + 1
            
         return count
+
+
+"""
+Notes:
+O(n) runtime.
+if the current number in the loop is greater than the next number then we add to count the difference between them plus 1
+since the next number needs to be one greater than the previous number. then we set the next number to one greater than the 
+previous number
+
+
+Constraints:
+1 <= nums.length <= 5000
+1 <= nums[i] <= 104
+
+
+"""
