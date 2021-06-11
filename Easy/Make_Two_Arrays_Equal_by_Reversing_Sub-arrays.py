d72fe9c6f2e6776e3821929dca45317a41322b3b
Jeremy Taraba <jeremytaraba@gmail.com>
2021-06-10 17:36:26 -0700

    Update Make_Two_Arrays_Equal_by_Reversing_Sub-arrays.py

--- Easy/Make_Two_Arrays_Equal_by_Reversing_Sub-arrays.py
+++ Easy/Make_Two_Arrays_Equal_by_Reversing_Sub-arrays.py
@@ -10,5 +10,14 @@ class Solution(object):
         return arr == target
 
 """
+Notes:
+We just need to see if one array contains the same numbers as the other. The easiest way to do this is to sort them
+and then compare them. We can use the built in sort function for python or we can make our own
+
+Constraints:
+target.length == arr.length
+1 <= target.length <= 1000
+1 <= target[i] <= 1000
+1 <= arr[i] <= 1000
 
 """
