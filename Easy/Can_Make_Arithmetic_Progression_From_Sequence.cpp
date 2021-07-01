216e48a33b4b56c27581488ef6dc1d517c832eb5
Jeremy Taraba <43662755+JeremyTaraba@users.noreply.github.com>
2021-06-30 23:13:46 -0700

    Update Can_Make_Arithmetic_Progression_From_Sequence.cpp

--- Easy/Can_Make_Arithmetic_Progression_From_Sequence.cpp
+++ Easy/Can_Make_Arithmetic_Progression_From_Sequence.cpp
@@ -5,7 +5,7 @@ Given an array of numbers arr. A sequence of numbers is called an arithmetic pro
 Return true if the array can be rearranged to form an arithmetic progression, otherwise, return false.
 
 Example:
-Input: arr = [3,5,1]
+Input: arr = [3,5,1] 
 Output: true
 Explanation: We can reorder the elements as [1,3,5] or [5,3,1] with differences 2 and -2 respectively, between each consecutive elements.
   
