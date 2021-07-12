8c4dc9b257647062a2a5af8f42928a9647fbc6a8
Jeremy Taraba <jeremytaraba@gmail.com>
2021-07-11 22:38:08 -0700

    Update Merge_Strings_Alternately.py

--- Easy/Merge_Strings_Alternately.py
+++ Easy/Merge_Strings_Alternately.py
@@ -9,8 +9,8 @@ Return the merged string.
 
 class Solution(object):
     def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
+        """  
+        :type word1: str 
         :type word2: str 
         :rtype: str  
         """
