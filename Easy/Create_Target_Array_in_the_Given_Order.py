fe4f6b86f5b786fcfe73de0e34ee8a9d81b04c6e
Jeremy Taraba <jeremytaraba@gmail.com>
2021-07-28 16:34:45 -0700

    Update Create_Target_Array_in_the_Given_Order.py

--- Easy/Create_Target_Array_in_the_Given_Order.py
+++ Easy/Create_Target_Array_in_the_Given_Order.py
@@ -1,3 +1,18 @@
+"""
+Prompt:
+Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+Initially target array is empty.
+From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+Repeat the previous step until there are no elements to read in nums and index.
+Return the target array.
+
+It is guaranteed that the insertion operations will be valid.
+
+
+"""
+
+
 class Solution(object):
     def createTargetArray(self, nums, index):
         """
@@ -5,6 +20,7 @@ class Solution(object):
         :type index: List[int]
         :rtype: List[int]
         """
+
         ans = []
         for i in range(len(nums)):
             ans.insert(index[i], nums[i])
