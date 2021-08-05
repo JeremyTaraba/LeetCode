3116eb94d38118d08413ae06166f75074022533b
Jeremy Taraba <43662755+JeremyTaraba@users.noreply.github.com>
2021-08-05 01:49:57 -0700

    Update Count_Items_Matching_A_Rule.cpp

--- Easy/Count_Items_Matching_A_Rule.cpp
+++ Easy/Count_Items_Matching_A_Rule.cpp
@@ -1,5 +1,5 @@
 /*
-Prompt:
+Prompt: 
 You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are 
 also given a rule represented by two strings, ruleKey and ruleValue.
 
