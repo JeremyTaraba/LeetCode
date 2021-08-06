9342d470df876245157bfb0b1e139f3cb2cbd0a8
Jeremy Taraba <43662755+JeremyTaraba@users.noreply.github.com>
2021-08-06 02:13:29 -0700

    Update Delete_Columns_To_Make_Sorted.cs

--- Easy/Delete_Columns_To_Make_Sorted.cs
+++ Easy/Delete_Columns_To_Make_Sorted.cs
@@ -1,5 +1,5 @@
 /*
-Prompt:
+Prompt: 
 You are given an array of n strings strs, all of the same length.
 
 The strings can be arranged such that there is one on each line, making a grid. For example, strs = ["abc", "bce", "cae"] can be arranged as:
