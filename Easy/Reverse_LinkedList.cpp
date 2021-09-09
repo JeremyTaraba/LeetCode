68e4efbec8321ea7bb9e3275bde25b249aa1e3fc
Jeremy Taraba <jeremytaraba@gmail.com>
2021-09-08 17:58:40 -0700

    Update Reverse_LinkedList.cpp

--- Easy/Reverse_LinkedList.cpp
+++ Easy/Reverse_LinkedList.cpp
@@ -6,7 +6,7 @@
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ * }; 
  */
 
 #include <bits/stdc++.h>
