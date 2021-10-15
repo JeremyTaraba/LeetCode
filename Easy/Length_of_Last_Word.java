16669d3f3fe05edf25be20d3eab3bb7c693c6b7b
Jeremy Taraba <jeremytaraba@gmail.com>
2021-10-14 21:25:34 -0700

    Update Length_of_Last_Word.java

--- Easy/Length_of_Last_Word.java
+++ Easy/Length_of_Last_Word.java
@@ -1,3 +1,13 @@
+/*
+Prompt:
+Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+
+*/
+
+
 class Solution {
     public int lengthOfLastWord(String s) {
         int length = 0;
