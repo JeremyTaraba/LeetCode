db5ffb59967e0d2dc5b958b821ca6f8c2049df59
Jeremy Taraba <jeremytaraba@gmail.com>
2021-10-15 21:34:13 -0700

    Update Single_Number.java

--- Easy/Single_Number.java
+++ Easy/Single_Number.java
@@ -1,10 +1,25 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
-        //even occurence will nullify
-        for(int i=0;i<nums.length;i++){
-            result ^= nums[i]; //Bitwise XOR 
+        int answer = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            answer = answer ^ nums[i]; //Bitwise XOR 
         }
-        return result;
+        return answer;
     }
 }
+
+/*
+Notes:
+Using a bitwise XOR we can add the bits if we have never seen it and subtract the bits if we have seen them.
+For example: 1, 3, 1. The answer should be 3
+    001 ^ 010 = 011
+    011 ^ 001 = 010
+    010 = 3
+
+
+Constraints:
+
+
+
+*/
