67f76191e281572d8e57e5e8fef708d0f2205a4e
Jeremy Taraba <jeremytaraba@gmail.com>
2021-11-07 00:11:06 -0700

    Update Generate_a_String_With_Characters_That_Have_Odd_Counts.py

--- Easy/Generate_a_String_With_Characters_That_Have_Odd_Counts.py
+++ Easy/Generate_a_String_With_Characters_That_Have_Odd_Counts.py
@@ -1,5 +1,5 @@
 """ 
-Prompt:
+Prompt: 
 Given an integer n, return a string with n characters such that each character in such string occurs an odd number of times.
 
 The returned string must contain only lowercase English letters. If there are multiples valid strings, return any of them.  
