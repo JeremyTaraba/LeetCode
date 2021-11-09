346bafcf6ef8cbde0fb1b7f8e19318a2493cc15b
Jeremy Taraba <jeremytaraba@gmail.com>
2021-11-08 20:28:39 -0800

    Update Final_Value_of_Variable_After_Performing_Operations.java

--- Easy/Final_Value_of_Variable_After_Performing_Operations.java
+++ Easy/Final_Value_of_Variable_After_Performing_Operations.java
@@ -1,3 +1,16 @@
+/*
+Prompt:
+There is a programming language with only four operations and one variable X:
+
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+*/
+
+
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int value = 0;
