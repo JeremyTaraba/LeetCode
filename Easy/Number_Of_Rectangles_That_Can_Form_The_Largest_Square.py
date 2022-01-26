f2e6ceab080ce205d1c41820f395cf73f4040313
Jeremy Taraba <jeremytaraba@gmail.com>
2022-01-25 16:16:13 -0800

    Update Number_Of_Rectangles_That_Can_Form_The_Largest_Square.py

--- Easy/Number_Of_Rectangles_That_Can_Form_The_Largest_Square.py
+++ Easy/Number_Of_Rectangles_That_Can_Form_The_Largest_Square.py
@@ -8,10 +8,10 @@ Let maxLen be the side length of the largest square you can obtain from any of t
 
 Return the number of rectangles that can make a square with a side length of maxLen.
  
-        
+         
 """    
  
- 
+  
 class Solution(object): 
     def countGoodRectangles(self, rectangles):
         """
