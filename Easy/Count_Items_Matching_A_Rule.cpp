b8003b423f1769c76d539154c67f2154128a665e
Jeremy Taraba <jeremytaraba@gmail.com>
2022-05-23 17:01:14 -0700

    Update Count_Items_Matching_A_Rule.cpp

--- Easy/Count_Items_Matching_A_Rule.cpp
+++ Easy/Count_Items_Matching_A_Rule.cpp
@@ -8,7 +8,7 @@ The ith item is said to match the rule if one of the following is true:
 ruleKey == "type" and ruleValue == typei.
 ruleKey == "color" and ruleValue == colori.
 ruleKey == "name" and ruleValue == namei.
-Return the number of items that match the given rule.
+Return the number of items that match the given rule. 
 
 Example:
 Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
