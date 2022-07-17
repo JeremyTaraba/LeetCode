02afe8daf6de0e534848720493465a4413ef1f14
Jeremy Taraba <jeremytaraba@gmail.com>
2022-07-17 00:22:59 -0700

    Update Binary_Search.java

--- Easy/Binary_Search.java
+++ Easy/Binary_Search.java
@@ -6,7 +6,7 @@ Given an array of integers nums which is sorted in ascending order, and an integ
 You must write an algorithm with O(log n) runtime complexity.
     
 */   
-    
+     
  
 class Solution {
     
