57af0658e0149937b12f5a868464b16dae86a088
Jeremy Taraba <jeremytaraba@gmail.com>
2023-01-23 17:19:45 -0800

    Create Container_With_Most_Water.py

---
+++ Easy/Container_With_Most_Water.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        # first attempt: don't brute force it, instead figure out a way to 
+        # find what is most likely the largest and then go from there
+        # so check largest bar with furthest bar and go from there
+
+        max = 0
+        left = 0
+        right = len(height) - 1
+        max = min((height[left], height[right])) * (right - left)
+        for i in range(len(height)):
+            if(min((height[left], height[right])) * (right - left) > max):
+                max = min((height[left], height[right])) * (right - left)
+            if(height[left] < height[right]):
+                left = left + 1
+            else:
+                right = right - 1
+      
+
+
+        return max
