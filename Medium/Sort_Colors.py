445bd7a8da50b74332a9c38f3ce51248a0f39711
Jeremy Taraba <jeremytaraba@gmail.com>
2023-02-01 19:56:53 -0800

    Second attempt

--- Medium/Sort_Colors.py
+++ Medium/Sort_Colors.py
@@ -24,3 +24,29 @@ class Solution:
             right = size - 1
 
         # ^ This ended up being O(n^2) selection sort
+
+
+
+        # Second attempt: --------------------------------------------------------------------------------
+        # Since we only have 0,1,2 as our numbers we can count 
+        # how many 0's, 1's, and 2's we have and then just replace the original list
+
+        # count 0's and 1's. Don't need to count 2's cause its whatever is left
+        num0 = 0
+        num1 = 0
+        for i in range(0,len(nums)):
+            if(nums[i] == 0):
+                num0 +=1
+            elif(nums[i] == 1):
+                num1 +=1
+            
+
+        for i in range(0,len(nums)):
+            if(num0 > 0):
+                nums[i] = 0
+                num0-=1
+            elif(num1 > 0):
+                nums[i] = 1
+                num1-=1
+            else:
+                nums[i] = 2
