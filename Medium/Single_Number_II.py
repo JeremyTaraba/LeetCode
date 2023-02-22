d9462c8b4a6022d47960846be97a0ae326f6a9d6
Jeremy Taraba <jeremytaraba@gmail.com>
2023-02-21 18:01:37 -0800

    Create Single_Number_II.py

---
+++ Medium/Single_Number_II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # First attempt: Can we turn it into a table that counts how many each one has occured?
+        # is 3 times important? what if it was 2 times? What if we add it up and subtract by 
+        # 3* each element? too complicated. Just use Counter to create a dictionary
+        ans = -1
+        collection = Counter(nums)
+
+        for i in collection:
+            if(collection[i] == 1):
+                ans = i
+        
+      
+                
+        return ans
