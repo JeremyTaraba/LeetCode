9c1f0dc84f581ba3c8a164dd3a8f44614c42f10c
Jeremy Taraba <jeremytaraba@gmail.com>
2023-05-25 19:21:05 -0700

    Update Contains_Duplicate.py

--- Easy/Contains_Duplicate.py
+++ Easy/Contains_Duplicate.py
@@ -1,7 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # first attempt: sort list and then check neighbors
-        # could also use set() and check if it already exists in the set
+        # first attempt: sort list and then check neighbors O(n log n)
+        # could also use set() and check if it already exists in the set O(n)
         s = set()
 
         for i in nums:
@@ -12,4 +12,12 @@ class Solution:
 
         return False
 
-        
+        # second attempt: Can turn list into dictionary and increment value and see if we already added it
+        # runtime is O(n) since we just go through the list once, similar to set
+        dict = {}
+        for n in nums:
+            dict[n] = dict.get(n, 0) + 1 # get the value if it exists and add 1 if not set it to 1          
+            if(dict[n]==2): # if value already exists is will be 2 or greater
+                return True
+       
+        return False
