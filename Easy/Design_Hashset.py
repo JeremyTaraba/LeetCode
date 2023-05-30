ee558a67167ab6cacda441884c13239ecd3adbc1
Jeremy Taraba <jeremytaraba@gmail.com>
2023-05-30 01:51:45 -0700

    Update Design_Hashset.py

--- Easy/Design_Hashset.py
+++ Easy/Design_Hashset.py
@@ -25,3 +25,30 @@ class MyHashSet:
         if(key in self.set):
             return True
         return False
+    
+
+    # Second attempt: Trying to use a hash for O(1) lookup, bad for memory but great runtime
+
+class MyHashSet:
+    size = 262144
+
+    def __init__(self):
+        self.set = [None] * self.size  #size of table is 2048, smaller size = more likely to collide
+        
+        
+    def hash_funct(self, key: int) -> int:
+        return hash(key) % self.size 
+
+    def add(self, key: int) -> None:
+        self.set[self.hash_funct(key)] = key     
+
+        
+
+    def remove(self, key: int) -> None:
+        self.set[self.hash_funct(key)] = None
+        
+
+    def contains(self, key: int) -> bool:
+        if self.set[self.hash_funct(key)] != None:
+            return True
+        return False
