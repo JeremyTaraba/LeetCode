a53e2b28e315b13dca782aa66a6c99d2d77f4954
Jeremy Taraba <jeremytaraba@gmail.com>
2023-06-29 21:15:55 -0700

    Update Candy.py

--- Hard/Candy.py
+++ Hard/Candy.py
@@ -34,4 +34,28 @@ class Solution:
 
         # ^ couldn't get it to work
 
-        # second attempt: Use 2 passes, will need another array. 
+   
+        # second attempt: Use 2 passes, will need another array to keep track of what we have given
+        # first give all kids 1 candy because they all get atleast one
+        # next loop and if 1 kid is greater than the one before it then give him an extra 1 candy of what the kid before got
+        # ex: 1,2,3 will be given 1,2,3 cadies
+        # ex: 1,2,3,3 will be given 1,2,3,1 candies since from 3 to 3 it doesnt increase
+        # Need a second pass for a case like 1,2,3,3,2 where they are given 1,2,3,2,1 
+
+        length = len(ratings)
+        arr = [1] * length
+
+        # first pass goes forwards
+        for i in range(length-1):       
+            if ratings[i] < ratings[i+1]:
+                arr[i+1] = arr[i] + 1
+                
+        
+        # second pass goes backwards 
+        for i in range(length-1, 0, -1):
+            if ratings[i-1] > ratings[i] and arr[i-1] <= arr[i]:        # make sure to check that we actually need to add 1; arr[i-1] could already be more than arr[i]
+                arr[i-1] = arr[i] + 1
+                
+                
+        
+        return sum(arr)
