acc477706dba24528422636607fddef368f6d9e7
Jeremy Taraba <jeremytaraba@gmail.com>
2023-07-10 17:36:26 -0700

    update

---
+++ Medium/Find_First_And_Last_Position_Of_Element_In_Sorted_Array.py
@@ -0,0 +1,34 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # first attempt: want to use binary search, when we found the target, move up and down 
+        # so we can find the beginning position and end position. Create a function that does binary search but
+        # returns the left most index so we can use it for left and use it for right + 1
+
+        def binarySearch(myTarget):
+            left = 0
+            right = len(nums)
+
+            while right > left:
+                i = int((right + left) // 2)
+                if nums[i] < myTarget:
+                    left = i+1
+                else:
+                    right = i           
+                    
+            return left
+
+        
+
+        begin = binarySearch(target)     
+        end = binarySearch(target+1)-1      # will give left most index of 1 more than target, so reduce it by 1 to get rightmost
+
+
+        
+        print(begin)
+        print(end)
+           
+
+        if begin > end:
+            return [-1,-1]
+
+        return [begin,end]
