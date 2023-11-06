63e279f3b8f29bd96ceaa8e33bca7b531e09c2e8
Jeremy Taraba <jeremytaraba@gmail.com>
2023-11-06 13:55:13 -0800

    Create N_Repeated_Element_In_Size_2N_Array.py

---
+++ Easy/N_Repeated_Element_In_Size_2N_Array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        # first attempt: use a dictionary to keep track of what we have seen so far
+        # only need to go through half of the array + 1 before we can check the dictionary for dupes
+        # checking for dupes could take O(n) time in worst case
+
+        dictionary = {}
+
+        size = len(nums)
+
+        for i in range(0,size//2+1):
+            if nums[i] in dictionary:
+                return nums[i]
+            else:
+                dictionary[nums[i]] = 1
+        return nums[size//2+1]
