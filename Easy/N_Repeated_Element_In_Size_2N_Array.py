c54008d59f673153155ed3583a1d3ab9313b0d3f
Jeremy Taraba <jeremytaraba@gmail.com>
2023-11-06 13:59:21 -0800

    Update N_Repeated_Element_In_Size_2N_Array.py

--- Easy/N_Repeated_Element_In_Size_2N_Array.py
+++ Easy/N_Repeated_Element_In_Size_2N_Array.py
@@ -3,6 +3,8 @@ class Solution:
         # first attempt: use a dictionary to keep track of what we have seen so far
         # only need to go through half of the array + 1 before we can check the dictionary for dupes
         # checking for dupes could take O(n) time in worst case, doesnt have to be dictionary, could be list
+        # another way to do this is using math, since you know the dupe will appear n times you can sum nums
+        # then sum the set of nums and subtract and divide by n to get the dupe. runtime and space arent any better tho
 
         dictionary = {}
 
