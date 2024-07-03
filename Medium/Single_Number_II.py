629c176e2b6945dd48afe6e635b7ba2744e885b0
Jeremy Taraba <jeremytaraba@gmail.com>
2024-07-03 15:45:49 -0700

    Update Single_Number_II.py

--- Medium/Single_Number_II.py
+++ Medium/Single_Number_II.py
@@ -1,21 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # First attempt: Can we turn it into a table that counts how many each one has occured?
-        # is 3 times important? what if it was 2 times? What if we add it up and subtract by 
-        # 3* each element? too complicated. Just use Counter to create a dictionary
-        ans = -1
+        # Counter is very useful for just counting occurrences 
+      
         collection = Counter(nums)
 
-        for i in collection:
-            if(collection[i] == 1):
-                ans = i
         
-      
-                
-        return ans
-    
-    # ^ runtime for Counter() is O(n)
-
-
-    # Second attempt:       ------------------------------------------------------------
-    # # Try doing a bitwise xor and & 
+        return collection.most_common()[-1][0]
