243aeaa429cffc91cc354fed7f6235a5a996f056
Jeremy Taraba <jeremytaraba@gmail.com>
2024-08-13 17:04:07 -0700

    Update Reverse_Linked_List.py

--- Easy/Reverse_Linked_List.py
+++ Easy/Reverse_Linked_List.py
@@ -4,6 +4,9 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    # First attempt: iterate through the linked list and add each element to a list, could also do a stack to be easier
+    # then go through that list in reverse order or go through the stack popping each element and adding them to a new ListNode instance
+    # runtime of O(n) and space of O(n) for the list/stack
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
             return head
