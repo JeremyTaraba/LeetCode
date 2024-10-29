8c400c02d5bfb1c83bbfb35c95943fa37dd91250
Jeremy Taraba <jeremytaraba@gmail.com>
2024-10-28 18:29:30 -0700

    Create Lowest_Common_Ancestor_Of_A_BST.py

---
+++ Medium/Lowest_Common_Ancestor_Of_A_BST.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # LCA is guaranteed so we have to return something
+        # bst means it is sorted!!!
+
+       cur = root
+       while cur:
+        if p.val > cur.val and q.val > cur.val:
+            cur = cur.right
+        elif p.val < cur.val and q.val < cur.val:
+            cur = cur.left
+        else: # if one is greater and one is les then that means cur is the node we want
+            return cur
+
