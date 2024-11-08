c54656987331742727236c5c2a3fa59bd3cb3c5e
Jeremy Taraba <jeremytaraba@gmail.com>
2024-11-07 23:14:17 -0800

    Update Plus_One.py

--- Easy/Plus_One.py
+++ Easy/Plus_One.py
@@ -8,7 +8,7 @@ class Solution:
         # don't have to reverse it could just start from the last index
 
         
-        digits.reverse()
+        digits.reverse()    # O(n)
         digits[0] +=1
         if digits[0] == 10:
             index = 0
@@ -21,3 +21,18 @@ class Solution:
                 digits.append(1)
         digits.reverse()
         return digits
+    
+
+
+    # verseion where we don't revese it. Runtime is the same
+    # digits[-1] +=1
+    #     if digits[-1] == 10:
+    #         index = len(digits)-1
+    #         while digits[index] == 10 and index > 0:
+    #             digits[index] = 0
+    #             digits[index-1] += 1
+    #             index-=1
+    #         if (0 == index and digits[0] == 10) or len(digits) == 1:
+    #             digits[index] = 0
+    #             digits.insert(0,1) # take O(n) since it has to push everything else back
+    #     return digits
