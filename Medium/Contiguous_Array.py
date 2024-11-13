ca01e3ba5500573a0c8f0f8587e101ff41f5f770
Jeremy Taraba <jeremytaraba@gmail.com>
2024-11-12 17:36:02 -0800

    Create Contiguous_Array.py

---
+++ Medium/Contiguous_Array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        res=0
+        count=0  # count of 1 minus count of zero
+        countToIndex = {0:-1} #index set to -1 before start to length calculation correctly
+        for i in range(len(nums)):
+            if nums[i] == 1 : 
+                count +=1
+            else: 
+                count -=1
+            # for equal zero and one, count = 0
+            #previously occured mean we found a subsequence 
+            #length = current - old index 
+            #between same count There is definitely a subsequence with equal count of 0,1 
+            if count in countToIndex:
+                res=max(res, i-countToIndex[count])
+                #wont update as oldest index have maximum subsequence
+            else:
+                #as not present we need to update this first occurance 
+                countToIndex[count] = i
+                
+        return res
