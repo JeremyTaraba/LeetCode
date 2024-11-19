13164d692f1ad7c1e5ad006ae754a4a73ffe213f
Jeremy Taraba <jeremytaraba@gmail.com>
2024-11-19 13:16:41 -0800

    Update Word_Break.py

--- Medium/Word_Break.py
+++ Medium/Word_Break.py
@@ -1,5 +1,8 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # these dynamic programming problems tend to have a map called dp and you either take a top down approach 
+        # or a bottom up approach. The bottom up approach is usually simpler and easier to understand.
+        # start from the last index and go to the first index, using memoization to reduce memory usage
         dp = [False] * (len(s) + 1)
         dp[len(s)] = True
 
