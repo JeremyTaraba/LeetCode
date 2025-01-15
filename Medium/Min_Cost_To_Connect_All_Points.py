1d914592efe5a792830487117ab78f4ac39afcda
Jeremy Taraba <jeremytaraba@gmail.com>
2025-01-14 18:15:45 -0800

    Create Min_Cost_To_Connect_All_Points.py

---
+++ Medium/Min_Cost_To_Connect_All_Points.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        # one way is to connect all points and then use djikstras to find shortest path
+        # another way is to sort points then connect based on neighbors?
+
+        # pre work for creating an adjacency list with every node connected to another ex: node = [cost, nextNode]
+        N = len(points)
+        adj = {i: [] for i in range(N)}
+        for i in range(N):
+            x1, y1 = points[i]
+            for j in range(i + 1, N):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj[i].append([dist, j])
+                adj[j].append([dist, i])
+
+        # prims algorithm for minimum spanning tree
+        res = 0
+        visit = set()
+        minH = [[0, 0]]
+        while len(visit) < N:
+            cost, i = heapq.heappop(minH)
+            if i in visit:
+                continue
+            res += cost
+            visit.add(i)
+            for neiCost, nei in adj[i]:
+                if nei not in visit:
+                    heapq.heappush(minH, [neiCost, nei])
+        return res
+
+        
